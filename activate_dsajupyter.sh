1523f95d7f85f683aae09d5022a20a5454041ba7
David Gutman <dagutman@gmail.com>
2016-09-23 20:43:07 +0000

    changing DSA Load Tools for girder compatibility

--- activate_dsajupyter.sh
+++ activate_dsajupyter.sh
@@ -1,1 +1,5 @@
-source  /home/dgutman/anaconda/envs/dsa_girder/bin/activate
+#!/bin/bash
+##ipcluster start --n=6 need to make this a supervisor process
+#source  /home/dgutman/anaconda/envs/dsa_girder/bin/activate
+source /home/dgutman/anaconda/envs/dsa_girder/bin/activate dsa_girder
+/home/dgutman/anaconda/envs/dsa_girder/bin/jupyter  notebook  --config=/home/dgutman/.jupyter/jupyter_notebook_config.py --notebook-dir=/home/dgutman/devel/CDSA_LoadTools/
