6b5f71cf86b8265459aefda992b1da8cdb10e16c
David Gutman <dagutman@gmail.com>
2018-07-06 15:45:56 -0400

    WIP: Adding scripts to process and upload DICOM and NIFTI Data

--- activate_dsajupyter.sh
+++ activate_dsajupyter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ##ipcluster start --n=6 need to make this a supervisor process
 #source  /home/dgutman/anaconda/envs/dsa_girder/bin/activate
-source /home/dgutman/anaconda/envs/dsa_girder/bin/activate dsa_girder
-/home/dgutman/anaconda/envs/dsa_girder/bin/jupyter  notebook  --config=/home/dgutman/.jupyter/jupyter_notebook_config.py --notebook-dir=/home/dgutman/devel/CDSA_LoadTools/
+source activate dsa_girder
+jupyter  notebook  --config=/home/dgutman/.jupyter/jupyter_notebook_config.py --notebook-dir=/home/dgutman/devel/CDSA_LoadTools/ --port 8899
